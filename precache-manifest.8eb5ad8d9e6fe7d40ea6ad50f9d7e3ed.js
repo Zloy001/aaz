f6c90279381fd181a874f53fa4266c6293dd1ca9
Zloy001 <qwer.01@mail.ru>
2020-07-19 13:30:41 +0600

    Updates

---
+++ precache-manifest.8eb5ad8d9e6fe7d40ea6ad50f9d7e3ed.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e598132197475f399442886a72a65fa1",
+    "url": "/aaz/index.html"
+  },
+  {
+    "revision": "23866b60b8f8f2b7489c",
+    "url": "/aaz/static/css/2.86df01db.chunk.css"
+  },
+  {
+    "revision": "ff7662e7aec629efc99b",
+    "url": "/aaz/static/css/main.aafe15a2.chunk.css"
+  },
+  {
+    "revision": "23866b60b8f8f2b7489c",
+    "url": "/aaz/static/js/2.ddfa5cd3.chunk.js"
+  },
+  {
+    "revision": "21ced859ea2b2d6b856d461ad6c2afed",
+    "url": "/aaz/static/js/2.ddfa5cd3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff7662e7aec629efc99b",
+    "url": "/aaz/static/js/main.00d04e16.chunk.js"
+  },
+  {
+    "revision": "0c7098ca93e23d3c0be7",
+    "url": "/aaz/static/js/runtime-main.e73e09e2.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/aaz/static/media/persik.4e1ec840.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.7ba8543416b27cef83bc8585256f4ad7.js"
+  "/aaz/precache-manifest.8eb5ad8d9e6fe7d40ea6ad50f9d7e3ed.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/aaz/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
