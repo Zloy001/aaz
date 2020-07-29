cda58158e6f97da7b9c5a3454210831bdb5c8068
Zloy001 <qwer.01@mail.ru>
2020-07-29 20:56:43 +0600

    Updates

--- precache-manifest.0b26e99c48d1b831fc1db554c13f8380.js
+++ precache-manifest.0b26e99c48d1b831fc1db554c13f8380.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "b7c0c866006c4bbfd1fbcaa8b6017ae3",
+    "revision": "896e16fb0134da785cd29604b3c19ce9",
     "url": "/aaz/index.html"
   },
   {
-    "revision": "bd029d644ca07a07fb0d",
+    "revision": "749c1771aee604f91421",
     "url": "/aaz/static/css/2.86df01db.chunk.css"
   },
   {
-    "revision": "420d1186e1f8de9e0bba",
+    "revision": "c7ebdf8f64cd1934c45c",
     "url": "/aaz/static/css/main.7f4b17eb.chunk.css"
   },
   {
-    "revision": "bd029d644ca07a07fb0d",
-    "url": "/aaz/static/js/2.91faaf76.chunk.js"
+    "revision": "749c1771aee604f91421",
+    "url": "/aaz/static/js/2.bebb98e5.chunk.js"
   },
   {
     "revision": "21ced859ea2b2d6b856d461ad6c2afed",
-    "url": "/aaz/static/js/2.91faaf76.chunk.js.LICENSE.txt"
+    "url": "/aaz/static/js/2.bebb98e5.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "420d1186e1f8de9e0bba",
-    "url": "/aaz/static/js/main.4e136ecf.chunk.js"
+    "revision": "c7ebdf8f64cd1934c45c",
+    "url": "/aaz/static/js/main.7dc27d26.chunk.js"
   },
   {
     "revision": "0c7098ca93e23d3c0be7",
